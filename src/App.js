bec4d7650ebe20894ecd067ec258b2aaeba9556b
shindeAkshu <sakshata416@gmail.com>
2022-07-26 12:23:14 -0700

    NavBar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { ReactDOM } from 'react';
 import './App.css';
-import './index.css';
+
 
 function App() {
   return (
-    <div>
+    <div className='App'>
+      <Navbar></Navbar>
     </div>
   );
 }
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import './app.css';
+
+class Navbar extends Component {
+    render() {
+      return(
+        <nav className="NavBarItem">
+            <h1 className="Navbar-logo">React</h1>
+            <div className="menu-icon">
+               
+            </div>
+            <ul>
+                <li><a href=""></a></li>
+            </ul>
+        </nav>
+      );
+    }
+       
+};
+
+export default Navbar;
