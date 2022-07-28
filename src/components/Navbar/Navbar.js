c81978b71ea8ac29183d70db87ccc7244b888f6b
shindeAkshu <sakshata416@gmail.com>
2022-07-28 11:16:09 -0700

    React NavBar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,19 +1,24 @@
 import React, { Component } from "react";
 import './app.css';
 import {MenuItems} from "./MenuItems";
+import './Navbar.css';
 
 class Navbar extends Component {
     render() {
       return(
         <nav className="NavBarItem">
-            <h1 className="Navbar-logo">React</h1>
+            <h1 className="Navbar-logo">React<i className="fab fa-react"></i></h1>
             <div className="menu-icon">
                
             </div>
             <ul>
                 {MenuItems.map((item,index) => {
                     return(
-                        <li><a className="{items.cName}"></a></li>
+                        <li key={index}>
+                          <a className={item.cName} href={item.url}>
+                          {item.title}  
+                          </a>
+                        </li>
                     )
               })}
                
