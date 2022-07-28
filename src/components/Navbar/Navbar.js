309e1b6754dcac436602e4ca1f48b6558f28ea07
shindeAkshu <sakshata416@gmail.com>
2022-07-28 13:05:22 -0700

    react navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,15 +1,16 @@
 import React, { Component } from "react";
-import './app.css';
+
 import {MenuItems} from "./MenuItems";
 import './Navbar.css';
 
 class Navbar extends Component {
+  state={clicked:flase}
     render() {
       return(
         <nav className="NavBarItem">
             <h1 className="Navbar-logo">React<i className="fab fa-react"></i></h1>
-            <div className="menu-icon">
-               
+            <div className="menu-icon"  onClick={this.handleClick}>
+               <i className={this.state.clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
             </div>
             <ul>
                 {MenuItems.map((item,index) => {
